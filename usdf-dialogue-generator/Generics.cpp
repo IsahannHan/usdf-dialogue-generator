e92ce8647076693fc90ac8ef5ca5e91cd9ae92a8
Isahann Hanacleto <isahanacleto@gmail.com>
2020-08-28 19:37:17 -0300

    Fixing generics functions

--- usdf-dialogue-generator/Generics.cpp
+++ usdf-dialogue-generator/Generics.cpp
@@ -3,16 +3,16 @@
 #include <iostream>
 
 template <typename T>
-const static std::string createMultipleItems(int nestedTabs, std::list<T>& list) {
-    std::string items;
-
-    for (auto p : list)
-        items.append(p.tag(nestedTabs));
-
-    return items;
+const static std::string createSingleItem(int nestedTabs, T& item){
+    return item.tag(nestedTabs);
 }
 
 template <typename T>
-const static std::string createSingleItem(int nestedTabs, T& item){
-    return item.tag(nestedTabs);
+const static std::string createMultipleItems(int nestedTabs, std::list<T>& list) {
+    std::string items;
+
+    for (auto i : list)
+        items.append(createSingleItem(nestedTabs, i));
+
+    return items;
 }
