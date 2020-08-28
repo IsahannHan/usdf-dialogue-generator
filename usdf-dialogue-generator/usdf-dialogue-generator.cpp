232bbb991ce5d1898e4b3a80358370c41bf64e06
Isahann Hanacleto <isahanacleto@gmail.com>
2020-08-28 19:57:08 -0300

    Visual studio stuff wtf

--- usdf-dialogue-generator/usdf-dialogue-generator.cpp
+++ usdf-dialogue-generator/usdf-dialogue-generator.cpp
@@ -1,21 +1,39 @@
 #include <fstream>
 #include <iostream>
+
 #include "Conversation.h"
+#include "Choice.h"
 
-using namespace std;
-
-ofstream of_file;
+std::ofstream of_file;
 
 int main(void) {
-    Page page("teste", "t", "foda-se", "caralho", 2, 355);
+    std::list<Choice> choices;
+    std::list<Page> pages;
 
-    Conversation conversation("Antonio pirocas", &page);
+    Cost cost(5, 10);
+    Ifitem ifitem(2, 993);
+
+    Choice choice("Name of the choice", &cost, true, "Yes message.", "No message", "Yoooooo", 5, 2, 1111, 22, 5, 7, 3, 99, false);
+    Choice choice1("WEEE HOO", &cost, true, "Yes.", "sage", "num sei", 5, 2, 3, 44, 5, 7, 8, 99, true);
+    Choice choice2("Name of the choiceSSSSSSSS", &cost, true, "SIMmessage.", "No", "Yoooooo", 5, 2, 111, 4, 5, 7, 8, 66, false);
+
+    choices.push_back(choice);
+    choices.push_back(choice1);
+    choices.push_back(choice2);
+
+    Page page("PAGE 1", "Panel test", "Voice oooof", "Test dialog", 1, 2, &ifitem, &choices);
+    Page page1("PAGE 2222", "SAM CEHOICES test", "Voice urgh", "Test testteste", 4, 6666, &ifitem, &choices);
+
+    pages.push_back(page);
+    pages.push_back(page1);
+
+    Conversation conversation("ednaldo pereira", &pages);
 
     // cout << "ACTOR: " << conversation.actor << "TAG: \n" << conversation.tag();
 
     // string _namespace;
 
-    of_file.open("generated_dialogue.txt", ios::out);
+    of_file.open("generated_dialogue.txt", std::ios::out);
 
     // cout << "--------------------STRIFE FILE GENERATOR--------------------\n\n";
     // cout << "*BEGGINING CREATION*\n";
@@ -23,18 +41,7 @@ int main(void) {
     // getline(cin, _namespace);
     // cout << "Namespace: " << _namespace << endl;
 
-    of_file << conversation.tag(0) << endl;
+    of_file << conversation.tag(0) << std::endl;
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
